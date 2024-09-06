846d7bc4d7d73fa049f6fe7651a6fcecc4b98d9c
Akash new <akashladwa64.in>
2024-09-06 07:46:59 +0530

    Problem3 Added of drill3

--- Tests/Test3.js
+++ Tests/Test3.js
@@ -1,5 +1,7 @@
-const inventory = require("../Data/Data3")
-const {problem1}=require(`../Solutions/solution3`)
+//Here I import the data from data file
+const inventory = require("../Data/Data3");
+//Here I Import the data from solution file
+const {problem1,problem3}=require(`../Solutions/solution3`)
 
 //TestCases of problem1
 
@@ -13,5 +15,12 @@ console.log("Argument pass is mandatory");
 let newcarinfo3=problem1(40);
 console.log("Incorrect Argument");
 
+// //TestCases of problem2
+// let oldcarinfo=problem2();
+// console.log(oldcarinfo);
 
+
+//TestCases of problem3
+newalphabet=problem3()
+console.log(newalphabet);
 
