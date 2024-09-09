55e2dd660650d2e2a07b4717404c14b73b6fcedf
Akash new <akashladwa64.in>
2024-09-09 10:03:05 +0530

    Function3 Added in drill4

--- Solutions/solution4.js
+++ Solutions/solution4.js
@@ -35,17 +35,23 @@ function map(elements, cb) {
   return newarr;
 }
 
-//  function reduce(elements, cb, startingValue) {
-// //   // Do NOT use .reduce to complete this function.
-// //   // How reduce works: A reduce function combines all elements into a single value going from left to right.
-// //   // Elements will be passed one by one into `cb` along with the `startingValue`.
-// //   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-// //   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
-
-//  for(let i=0 ;i<elements.length ;i++){
-//     cb(startingValue,elements[i])
-//  }  
-// }
+ function reduce(elements, cb, startingValue) {
+//   // Do NOT use .reduce to complete this function.
+//   // How reduce works: A reduce function combines all elements into a single value going from left to right.
+//   // Elements will be passed one by one into `cb` along with the `startingValue`.
+//   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
+//   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+let show=startingValue;
+ for(let i=0 ;i<elements.length ;i++){
+   if(show===undefined){
+    show=elements[i];
+   }
+   else{
+    show=cb(show,elements[i]);
+   }
+}  
+return show;
+}
 
  function find(elements, cb) {
 //   // Do NOT use .includes, to complete this function.
@@ -61,11 +67,13 @@ for(let i=0;i<elements.length;i++){
    return undefined;
 }
 
-// function filter(elements, cb) {
+ function filter(elements, cb) {
 //   // Do NOT use .filter, to complete this function.
 //   // Similar to `find` but you will return an array of all elements that passed the truth test
 //   // Return an empty array if no elements pass the truth test
-// }
+
+
+}
 
 // const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 
@@ -142,4 +150,4 @@ for(let i=0;i<elements.length;i++){
 //   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 
 // }
-module.exports={each,map,find};
+module.exports={each,map,find,reduce};
